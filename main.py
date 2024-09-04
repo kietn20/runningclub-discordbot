52cc2b689589eb557525b97a7a8a5b1e4003af1d
kietn20 <kietnguyen369*@gmail.com>
2024-09-03 17:07:49 -0700

    done with main

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+from config import TOKEN, PREFIX
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    
+# Load cogs
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+bot.run(TOKEN)
